acb5ef8aa5b1f39bbeb14f24b29cfcfe2106e0be
heonseopShin <shinsub6120@gmail.com>
2021-06-21 01:28:52 +0900

    coinChange, rotateImage 설명 추가

--- src/CoinChange.kt
+++ src/CoinChange.kt
@@ -35,18 +35,29 @@ import java.lang.Integer.min
     Memory Usage: 62.4 MB, less than 8.16% of Kotlin online submissions for Coin Change.
  */
 fun coinChange(coins: IntArray, amount: Int): Int {
+    /*최대값이 10000이어서 혹시몰라 10001로 전체를 초기화 합니다.
+      해당 코드는 int 형 배열을 amount+1 크기로 초기화
+      (우리의 목적은 amount일때 코인값이기 때문에 +1)
+    * */
     val dp = Array<Int>(amount + 1) { 10001 }
-    println(dp.contentDeepToString())
 
     dp[0] = 0
+    //1<= x <= amount
     for (checkAmount in 1..amount) {
+        //코인들의 배열을 순회합니다.
         for (coin in coins) {
+            /*현재 값기준에서 coin값이 적다면
+              즉, 코인을 변환할 수 있다면
+             */
             if (coin <= checkAmount) {
+                /*
+                   현재 값기준으로 최소로 변환 할수 있는 값을 구합니다.
+                 */
                 dp[checkAmount] = min(dp[checkAmount], dp[checkAmount - coin] + 1)
             }
         }
     }
-
+    //최종 목적지 값이 변경 안됬을 경우는 요청사항에 따라 -1 로 변경합니다.
     return if (dp[amount] == 10001) -1 else dp[amount]
 }
 
--- src/rotateImage.kt
+++ src/rotateImage.kt
@@ -12,7 +12,10 @@ fun rotate(matrix: Array<IntArray>) {
 
 fun transpose(matrix: Array<IntArray>) {
     val matrixSize = matrix.size
+    println(matrix.contentDeepToString())
+    //0<=i<matrixSize
     for (i in 0 until matrixSize) {
+        //i<=j<matrixSize
         for (j in i until matrixSize) {
             val temp = matrix[j][i]
             matrix[j][i] = matrix[i][j]
@@ -38,8 +41,11 @@ fun reflect(matrix: Array<IntArray>) {
  */
 fun rotate2(matrix: Array<IntArray>) {
     val matrixSize = matrix.size
+    //0 <= i < (matrixSize + 1)/2 (홀짝 관계없도록 +1 사전 추가)
     for ( i in 0 until (matrixSize + 1)/2){
+        //0 <= i < (matrixSize)/2
         for ( j in 0 until (matrixSize / 2)){
+            //swap을 위해 테두리 값중 하나를 저장 이후 각 테두리 위치 이동
             val temp = matrix[matrixSize - 1 - j][i]
             matrix[matrixSize - 1 - j][i] = matrix[matrixSize - 1 - i][matrixSize - j - 1]
             matrix[matrixSize - 1 - i][matrixSize - j - 1] = matrix[j][matrixSize - 1 - i]
@@ -47,7 +53,29 @@ fun rotate2(matrix: Array<IntArray>) {
             matrix[i][j] = temp
         }
     }
-    println(matrix.contentDeepToString())
+}
+/*
+    Runtime: 168 ms, faster than 80.88% of Kotlin online submissions for Rotate Image.
+    Memory Usage: 35.4 MB, less than 59.31% of Kotlin online submissions for Rotate Image.
+ */
+fun rotate3(matrix: Array<IntArray>){
+    val matrixSize = matrix.size
+    //matrix와 동일한 사이즈로 초기화
+    val temp = Array(matrixSize){Array(matrixSize){0} }
+
+    //temp에 행과 열을 바꾸는 방식으로 값 변경
+    for (i in 0 until matrixSize){
+        for (j in 0 until matrixSize){
+            temp[j][i] = matrix[matrixSize-1-i][j]
+        }
+    }
+
+    //temp의 값을 기존 matrix에 복사
+    for (i in 0 until matrixSize){
+        for (j in 0 until matrixSize){
+            matrix[j][i] = temp[j][i]
+        }
+    }
 }
 
 fun main() {
@@ -62,4 +90,10 @@ fun main() {
         intArrayOf(13, 3, 6, 7), intArrayOf(15, 14, 12, 16)
     )
     rotate2(matrix2)
+
+    val matrix3: Array<IntArray> = arrayOf(
+        intArrayOf(5, 1, 9, 11), intArrayOf(2, 4, 8, 10),
+        intArrayOf(13, 3, 6, 7), intArrayOf(15, 14, 12, 16)
+    )
+    rotate3(matrix3)
 }
