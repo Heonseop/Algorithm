5f5d8b7f0f943a25b5f3c3d373987e076e2dd67a
heonseopShin <shinsub6120@gmail.com>
2022-01-19 00:27:21 +0900

    backjoon algorithm 1182

---
+++ src/backjoon/SumOfPartialSequences.java
@@ -0,0 +1,40 @@
+package backjoon;
+
+import java.util.Scanner;
+
+class SumOfPartialSequences {
+    static int N;
+    static int S;
+    static int result;
+    static int[] arr;
+
+    static void solution(int k, int currentNum) {
+        if (k == N) {
+            if (currentNum == S) {
+                result++;
+            }
+        } else {
+            //현재 값을 선택한 경우
+            solution(k + 1, currentNum + arr[k]);
+
+            //현재 값을 선택하지 않은 경우
+            solution(k + 1, currentNum);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        S = sc.nextInt();
+        arr = new int[N];
+        result = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        solution(0, 0);
+        if (S == 0) {
+            result--;
+        }
+        System.out.println(result);
+    }
+}
---
+++ src/backjoon/SumOfPartialSequencesKt.kt
@@ -0,0 +1,37 @@
+package backjoon
+
+import java.util.*
+
+var N = 0
+var S = 0
+var result = 0
+private var arr: IntArray = intArrayOf()
+
+private fun solution(k: Int, currentNum: Int) {
+    if (k == N) {
+        if (currentNum == S) {
+            result++
+        }
+    } else {
+        //현재 값을 선택한 경우
+        solution(k + 1, currentNum + arr[k])
+
+        //현재 값을 선택하지 않은 경우
+        solution(k + 1, currentNum)
+    }
+}
+
+fun main() = with(Scanner(System.`in`)) {
+    N = nextInt()
+    S = nextInt()
+    arr = IntArray(N)
+    result = 0
+    for (i in 0 until N) {
+        arr[i] = nextInt()
+    }
+    solution(0, 0)
+    if (S == 0) {
+        result--
+    }
+    println(result)
+}
